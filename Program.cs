af926608849fc775905028caff9ac6933201abc0
KellyCristinaAires <k.woys@hotmail.com>
2020-09-27 16:32:44 -0300

    atividadE formula Heron

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace formulaheron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A;
+            double B;
+            double C;
+            double perimetro;
+            double area;
+             
+             Console.Write("Digite o valor A: ");
+             A = double.Parse(Console.ReadLine());
+
+             Console.Write("Digite o valor de B: ");
+             B = double.Parse(Console.ReadLine());
+
+             Console.Write("Digite o valor de C: ");
+             C = double.Parse( Console.ReadLine());
+            Console.Write("\n");
+
+            perimetro = (A + B + C ) / 2;
+            area = Math.Sqrt(perimetro * (perimetro - A) * (perimetro - B) * (perimetro - C ));
+
+           Console.WriteLine("Semiperimetro = {0}",perimetro);
+           Console.WriteLine("Area = {0}(cm²)", A);
+           Console.Write("\n");
+
+           Console.WriteLine("Pressione ENTER sair!");
+           Console.ReadKey();
+        }
+    }
+}
